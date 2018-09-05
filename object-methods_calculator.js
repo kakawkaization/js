958edaf1e4eeea067d2599b9fd1d888757373562
kakawkaization <kakawkaization@gmail.com>
2018-09-05 16:40:46 +0300

    object-methods ladder

---
+++ object-methods_calculator.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var calculator = {
+	read: function() {
+		this.a = +prompt("a?", "");
+		this.b = +prompt("b?", "");
+	},
+
+	sum: function() {
+		return this.a + this.b;
+	},
+
+	mul: function() {
+		return this.a * this.b;
+	}
+}
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
