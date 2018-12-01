7353bf8005fe33974312e4bf58f58f13d1aba441
kakawkaization <kakawkaization@gmail.com>
2018-12-01 23:16:22 +0300

    5.2

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
 "use strict";
 
-function Calculator(stringOp) {
-	var arr = [];
-
-	arr = stringOp.split(" ");
-
-	for(var i = 0; i < arr.length; i++) {
-		if(arr[i] == "+") {
-			return +(arr[0]) + +(arr[2]);
-		}
-		else if(arr[i] == "-") {
-			return +(arr[0]) - +(arr[2]);
-		}
-	}
-}
-
-alert(Calculator("1 - 2"));
+for(var i = 0; i < 5; i++) {}
