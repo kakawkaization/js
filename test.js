e44d923f53f907a061ad60718b44c682328a1cce
kakawkaization <kakawkaization@gmail.com>
2018-12-03 22:40:40 +0300

    function properties

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 "use strict";
 
-function makeCounter()
+function makeCounter() {
+    var currentCount = 1;
+
+    return function () {
+        return currentCount++;
+    };
+}
+
+var counter = makeCounter();
+
+alert(counter());
+alert(counter());
+alert(counter());
+
+var counter2 = makeCounter();
+
+alert(counter2());
